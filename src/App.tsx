9b26c9686649ea2fedd39ce61370760a52d325e2
Thomas Allan <88735814+ThomOliver@users.noreply.github.com>
2025-12-17 11:43:27 -0300

    Fix- erro render https

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
 import { useChatStore } from './store/chat.store';
+import { useEffect } from 'react';
 
 export default function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/', { replace: true });
+  }, []);
+
   const username = useChatStore((state) => state.username);
 
   return (
